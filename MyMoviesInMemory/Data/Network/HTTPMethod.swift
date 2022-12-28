03c70a4bb22cfea94b936a5e6f99b0d8f7a0800a
JoSH0318 <jsh9870@naver.com>
2022-12-29 03:55:42 +0900

    feat: enum HTTPMethod 구현

---
+++ MyMoviesInMemory/Data/Network/HTTPMethod.swift
@@ -0,0 +1,30 @@
+//
+//  HTTPMethod.swift
+//  MyMoviesInMemory
+//
+//  Created by 조성훈 on 2022/12/28.
+//
+
+import Foundation
+
+enum HTTPMethod {
+  case get
+  case post
+  case patch
+  case delete
+}
+
+extension HTTPMethod {
+    var description: String {
+        switch self {
+        case .get:
+            return "GET"
+        case .post:
+            return "POST"
+        case .patch:
+            return "PATCH"
+        case .delete:
+            return "DELETE"
+        }
+    }
+}
