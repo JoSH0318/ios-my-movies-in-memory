f74202d4efb8dab9c721943ac918ee350eafde01
JoSH0318 <jsh9870@naver.com>
2022-12-29 19:01:54 +0900

    feat: MovieUseCase 구현

---
+++ MyMoviesInMemory/Domain/UseCases/MovieUseCase.swift
@@ -0,0 +1,21 @@
+//
+//  MovieUseCase.swift
+//  MyMoviesInMemory
+//
+//  Created by 조성훈 on 2022/12/29.
+//
+
+import Foundation
+import RxSwift
+
+final class MovieUseCase: MovieUseCaseType {
+    private let repository: NetworkRepository
+    
+    init(repository: NetworkRepository) {
+        self.repository = repository
+    }
+    
+    func fetchMovies(title: String) -> Observable<[Movie]> {
+        return repository.fetchMovies(title: title)
+    }
+}
