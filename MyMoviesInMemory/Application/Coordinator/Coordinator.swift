853dd17a91a009715c02ff9b4d715395ab3a43fc
JoSH0318 <jsh9870@naver.com>
2022-12-29 19:11:19 +0900

    feat: scene을 관리하는 Coordinator protocol 구현

---
+++ MyMoviesInMemory/Application/Coordinator/Coordinator.swift
@@ -0,0 +1,20 @@
+//
+//  Coordinator.swift
+//  MyMoviesInMemory
+//
+//  Created by 조성훈 on 2022/12/29.
+//
+
+import UIKit
+
+protocol Coordinator: AnyObject {
+    var navigationController: UINavigationController { get }
+    var parentCoordinators: Coordinator? { get set }
+    var childCoordinators: [Coordinator] { get set }
+}
+
+extension Coordinator {
+    func removeChildCoordinator(child: Coordinator) {
+        childCoordinators.removeAll()
+    }
+}
