b1dce92f104681abdb0554b549c92d3e1ac8125a
JoSH0318 <jsh9870@naver.com>
2023-01-05 02:42:09 +0900

    feat:  View test를 위한 DummyMovies 구현

---
+++ MyMoviesInMemory/Presentation/Home/View/DummyMovies.swift
@@ -0,0 +1,21 @@
+//
+//  DummyMovies.swift
+//  MyMoviesInMemory
+//
+//  Created by 조성훈 on 2022/12/30.
+//
+
+import Foundation
+
+struct DummyMovies {
+    let dummy = Movie(
+        title: "스타워즈",
+        id: "test.com",
+        subtitle: "The Last Jedi",
+        imageUrl: "https://ssl.pstatic.net/imgmovie/mdi/mit110/1254/125488_P20_135324.jpg",
+        openingYear: "2017",
+        director: "라이언 존슨",
+        actors: "데이지 리들리 | 테스터",
+        userRating: "6.32"
+    )
+}
