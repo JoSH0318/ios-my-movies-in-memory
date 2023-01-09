ffde122ec3eeb85b853b138e9106a18a56635672
JoSH0318 <jsh9870@naver.com>
2023-01-10 00:02:35 +0900

    feat: CoreData의 Domain entity인 Review data model 구현

---
+++ MyMoviesInMemory/Domain/Entity/Review.swift
@@ -0,0 +1,21 @@
+//
+//  Review.swift
+//  MyMoviesInMemory
+//
+//  Created by 조성훈 on 2023/01/09.
+//
+
+import Foundation
+
+struct Review {
+    let title: String
+    let id: String
+    let subtitle: String
+    let imageUrl: String
+    let openingYear: String
+    let director: String
+    let actors: String
+    let userRating: Double
+    let personalRating: Double
+    let commentary: String
+}
