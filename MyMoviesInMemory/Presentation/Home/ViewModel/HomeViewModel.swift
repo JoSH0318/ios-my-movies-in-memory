f4f3638b8f19800649bcd0cf9c27c31a06f0e69c
JoSH0318 <jsh9870@naver.com>
2023-01-09 18:45:02 +0900

    refactor: In/Output, transform 구현

--- MyMoviesInMemory/Presentation/Home/ViewModel/HomeViewModel.swift
+++ MyMoviesInMemory/Presentation/Home/ViewModel/HomeViewModel.swift
@@ -6,11 +6,47 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
 
 final class HomeViewModel {
-    private let movieUseCase: MovieUseCaseType
     
-    init(movieUseCase: MovieUseCaseType) {
-        self.movieUseCase = movieUseCase
+    // MARK: - Input
+    
+    struct Input {
+        let viewDidLoadEvent: Observable<Void>
+        let didTapRefreshButton: Observable<Void>
+    }
+    
+    // MARK: - Output
+    
+    struct Output {
+        let reviews: Observable<[MovieDAO]>
+    }
+    
+    // MARK: - Properties
+    
+    private let reviewUseCase: ReviewUseCaseType
+    
+    // MARK: - Initializer
+    
+    init(reviewUseCase: ReviewUseCaseType) {
+        self.reviewUseCase = reviewUseCase
+    }
+    
+    // MARK: - Methods
+    
+    func transform(_ input: Input) -> Output {
+        let reviews = Observable
+            .merge(
+                input.viewDidLoadEvent,
+                input.didTapRefreshButton
+            )
+            .withUnretained(self)
+            .flatMap { owner, _ in
+                owner.reviewUseCase.fetchReviews()
+            }
+        
+        return Output(reviews: reviews)
     }
 }
