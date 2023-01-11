a37d1be59a22c43db57c3fcc7a4a09344e70b0e7
JoSH0318 <jsh9870@naver.com>
2023-01-12 00:44:13 +0900

    fea: extension UIColor 구현

---
+++ MyMoviesInMemory/Util/Extension/UIColor.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor.swift
+//  MyMoviesInMemory
+//
+//  Created by 조성훈 on 2023/01/12.
+//
+
+import UIKit
+
+extension UIColor {
+    static let MBeige = UIColor(named: "MBeige")
+    static let MGray = UIColor(named: "MGray")
+    static let MGreen = UIColor(named: "MGreen")
+    static let MWhite = UIColor(named: "MWhite")
+}
