1fca3504f52754fca7dab498b3bec06daac10c47
JoSH0318 <jsh9870@naver.com>
2023-01-13 01:24:15 +0900

    feat: MovieReviewCellViewModelItem 구현

---
+++ MyMoviesInMemory/Presentation/Home/View/Cell/ViewModel/MovieReviewCellViewModelItem.swift
@@ -0,0 +1,21 @@
+//
+//  MovieReviewCellViewModelItem.swift
+//  MyMoviesInMemory
+//
+//  Created by 조성훈 on 2023/01/12.
+//
+
+import Foundation
+
+struct MovieReviewCellViewModelItem {
+    let title: String
+    let id: String
+    let subtitle: String
+    let openingYear: String
+    let director: String
+    let actors: String
+    let userRating: String
+    let personalRating: String
+    let commentary: String
+    let recordDate: String
+}
