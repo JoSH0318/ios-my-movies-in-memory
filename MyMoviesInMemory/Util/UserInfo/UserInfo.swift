dd16f95704eec6ea6a91464c8f27a8c765019763
JoSH0318 <jsh9870@naver.com>
2023-01-13 22:06:31 +0900

    feat: UserInfo 구현

---
+++ MyMoviesInMemory/Util/UserInfo/UserInfo.swift
@@ -0,0 +1,14 @@
+//
+//  UserInfo.swift
+//  MyMoviesInMemory
+//
+//  Created by 조성훈 on 2023/01/13.
+//
+
+import Foundation
+
+struct UserInfo {
+    static let naverClientId = "Tkn0wA2s08hupRMoxlvP"
+    static let naverClientSecret = "Bre9iIzgOZ"
+    static let OMDbClientKey = "f5a69f4d"
+}
