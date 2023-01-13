fc4932a512f00a66a6f2253b56743744930e6b07
JoSH0318 <jsh9870@naver.com>
2023-01-13 21:27:38 +0900

    feat: extension UIImage 구현

---
+++ MyMoviesInMemory/Util/Extension/UIImage.swift
@@ -0,0 +1,12 @@
+//
+//  UIImage.swift
+//  MyMoviesInMemory
+//
+//  Created by 조성훈 on 2023/01/13.
+//
+
+import UIKit
+
+extension UIImage {
+    static let linerCode = UIImage(named: "LinerCode")
+}
