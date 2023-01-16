fb1060b51f52ada9faa85ab047a69c1f5b7483d1
JoSH0318 <jsh9870@naver.com>
2023-01-16 22:02:54 +0900

    feat: SearchCoordinator 구현

--- MyMoviesInMemory/Presentation/Search/Coordinator/SearchCoordinator.swift
+++ MyMoviesInMemory/Presentation/Search/Coordinator/SearchCoordinator.swift
@@ -5,6 +5,30 @@
 //  Created by 조성훈 on 2023/01/14.
 //
 
-import Foundation
+import UIKit
 
-final class SearchCoordinator {}
+final class SearchCoordinator: Coordinator {
+    var navigationController: UINavigationController
+    var parentCoordinators: Coordinator?
+    var childCoordinators: [Coordinator] = []
+    private let useCase: MovieUseCaseType
+    
+    init(
+        navigationController: UINavigationController,
+        parentCoordinators: Coordinator,
+        useCase: MovieUseCaseType
+    ) {
+        self.navigationController = navigationController
+        self.parentCoordinators = parentCoordinators
+        self.useCase = useCase
+    }
+    
+    func start() {
+        let searchViewModel = SearchViewModel(movieUseCase: useCase)
+        let searchViewController = SearchViewController(searchViewModel)
+        self.navigationController.pushViewController(
+            searchViewController,
+            animated: true
+        )
+    }
+}
