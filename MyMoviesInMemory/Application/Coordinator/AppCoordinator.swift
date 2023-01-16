6f2c16b69edcda2d2466ed9eefa9ad82d67515a5
JoSH0318 <jsh9870@naver.com>
2023-01-16 22:03:55 +0900

    refactor: tabBarController를 추가로 인한 코드 수정

--- MyMoviesInMemory/Application/Coordinator/AppCoordinator.swift
+++ MyMoviesInMemory/Application/Coordinator/AppCoordinator.swift
@@ -11,21 +11,58 @@ final class AppCoordinator: Coordinator {
     var navigationController: UINavigationController
     var parentCoordinators: Coordinator?
     var childCoordinators: [Coordinator] = []
-    private let useCase: ReviewUseCaseType = ReviewUseCase(
+    private let reviewUseCase: ReviewUseCaseType = ReviewUseCase(
         repository: ReviewRepository(coreDataManager: CoreDataManager()))
+    private let movieUseCase: MovieUseCaseType = MovieUseCase(
+        repository: MovieRepository(networkProvider: MovieNetworkProvider()))
     
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
     
     func start() {
+        let tabBarController = UITabBarController()
+        tabBarController.tabBar.backgroundColor = UIColor.MGreen
+        tabBarController.tabBar.tintColor = UIColor.MWhite
+        
+        let homeNavigationController = UINavigationController()
+        homeNavigationController.tabBarItem = UITabBarItem(
+            title: "home",
+            image: UIImage(systemName: "film"),
+            tag: 0
+        )
+        homeNavigationController.navigationBar.tintColor = .label
+        
+        let searchNavigationController = UINavigationController()
+        searchNavigationController.tabBarItem = UITabBarItem(
+            title: "search",
+            image: UIImage(systemName: "magnifyingglass"),
+            tag: 1
+        )
+        searchNavigationController.navigationBar.tintColor = .label
+        
+        tabBarController.viewControllers = [
+            homeNavigationController,
+            searchNavigationController
+        ]
+        navigationController.viewControllers = [tabBarController]
+        navigationController.setNavigationBarHidden(true, animated: false)
+        
         let homeCoordinator = HomeCoordinator(
-            navigationController: self.navigationController,
+            navigationController: homeNavigationController,
             parentCoordinators: self,
-            useCase: self.useCase
+            useCase: self.reviewUseCase
         )
         
-        self.childCoordinators.append(homeCoordinator)
+        let searchCoordinator = SearchCoordinator(
+            navigationController: searchNavigationController,
+            parentCoordinators: self,
+            useCase: self.movieUseCase
+        )
+        
+        childCoordinators.append(homeCoordinator)
+        childCoordinators.append(searchCoordinator)
         homeCoordinator.start()
+        searchCoordinator.start()
     }
 }
