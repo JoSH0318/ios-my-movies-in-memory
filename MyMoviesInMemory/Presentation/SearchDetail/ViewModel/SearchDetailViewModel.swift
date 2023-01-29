ff0a3287ed5b7253a2589257a59df55650ca56da
JoSH0318 <jsh9870@naver.com>
2023-01-29 18:34:41 +0900

    refactor: posterImage 관련 로직 추가

--- MyMoviesInMemory/Presentation/SearchDetail/ViewModel/SearchDetailViewModel.swift
+++ MyMoviesInMemory/Presentation/SearchDetail/ViewModel/SearchDetailViewModel.swift
@@ -19,30 +19,43 @@ final class SearchDetailViewModel {
     // MARK: - Output
     
     struct Output {
+        let posterImage: Observable<UIImage?>
         let movie: Observable<Movie>
     }
     
     // MARK: - Properties
     
     private let movieUseCase: MovieUseCaseType
+    private let imageManager = ImageManager.shared
+    private let downloadTaskToken: UInt
     private let movie: Movie
     
     // MARK: - Initializer
     
     init(movieUseCase: MovieUseCaseType, movie: Movie) {
         self.movieUseCase = movieUseCase
+        self.downloadTaskToken = imageManager.nextToken()
         self.movie = movie
     }
     
     // MARK: - Methods
     
     func transform(_ input: Input) -> Output {
+        let posterImage = input.didShowView
+            .withUnretained(self)
+            .flatMap { owner, movie in
+                owner.imageManager.downloadImage(
+                    owner.movie.posterPath,
+                    owner.downloadTaskToken
+                )
+            }
+        
         let output = input.didShowView
             .withUnretained(self)
             .flatMap { owner, _ in
                 Observable.just(owner.movie)
             }
         
-        return Output(movie: output)
+        return Output(posterImage: posterImage, movie: output)
     }
 }
