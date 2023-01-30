0619ce479628bd893cf39873e7c9796967483b3a
JoSH0318 <jsh9870@naver.com>
2023-01-30 20:34:21 +0900

    feat: EditViewController 초기 구현

---
+++ MyMoviesInMemory/Presentation/Edit/View/EditViewController.swift
@@ -0,0 +1,42 @@
+//
+//  EditViewController.swift
+//  MyMoviesInMemory
+//
+//  Created by 조성훈 on 2023/01/30.
+//
+
+import UIKit
+import RxSwift
+
+class EditViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    private let viewModel: EditViewModel
+    private let coordinator: EditCoordinator
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - Initializer
+    
+    init(
+        _ viewModel: EditViewModel,
+        _ coordinator: EditCoordinator
+    ) {
+        self.viewModel = viewModel
+        self.coordinator = coordinator
+        super.init(nibName: nil, bundle: nil)
+        
+        bind()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // MARK: - Methods
+    func bind() {}
+}
