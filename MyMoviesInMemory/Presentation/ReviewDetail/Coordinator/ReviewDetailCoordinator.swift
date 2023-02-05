c78ee9dd17e2333982252c7cfbb34a91b42ec088
JoSH0318 <jsh9870@naver.com>
2023-02-05 23:13:14 +0900

    refactor: ReviewDetailCoordinator 수정

--- MyMoviesInMemory/Presentation/ReviewDetail/Coordinator/ReviewDetailCoordinator.swift
+++ MyMoviesInMemory/Presentation/ReviewDetail/Coordinator/ReviewDetailCoordinator.swift
@@ -5,8 +5,39 @@
 //  Created by 조성훈 on 2023/01/17.
 //
 
-import Foundation
+import UIKit
 
-//final class ReviewDetailCoordinator: Coordinator {
-//    
-//}
+final class ReviewDetailCoordinator: Coordinator {
+    var navigationController: UINavigationController
+    var parentCoordinator: Coordinator?
+    var childCoordinators: [Coordinator] = []
+    private let reviewUseCase: ReviewUseCaseType
+    
+    init(
+        navigationController: UINavigationController,
+        parentCoordinator: Coordinator,
+        reviewUseCase: ReviewUseCaseType
+    ) {
+        self.navigationController = navigationController
+        self.parentCoordinator = parentCoordinator
+        self.reviewUseCase = reviewUseCase
+    }
+    
+    func start(with review: Review, _ posterImage: UIImage?) {
+        let reviewDetailViewModel = ReviewDetailViewModel(
+            reviewUseCase: reviewUseCase,
+            review: review,
+            posterImage: posterImage
+        )
+        
+        let reviewDetailViewController = ReviewDetailViewController(
+            reviewDetailViewModel,
+            self
+        )
+        
+        self.navigationController.pushViewController(
+            reviewDetailViewController,
+            animated: true
+        )
+    }
+}
