6cf4c66648de107f5a6f5e3f16b0124ef93b2b55
JoSH0318 <jsh9870@naver.com>
2023-02-05 23:13:50 +0900

    feat: ReviewDetailVC, VM 초기 구현

--- MyMoviesInMemory/Presentation/ReviewDetail/View/ReviewDetailViewController.swift
+++ MyMoviesInMemory/Presentation/ReviewDetail/View/ReviewDetailViewController.swift
@@ -6,8 +6,31 @@
 //
 
 import UIKit
+import RxSwift
 
 final class ReviewDetailViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let viewModel: ReviewDetailViewModel
+    private var disposeBag = DisposeBag()
+    private let coordinator: ReviewDetailCoordinator
+    
+    // MARK: - Initializer
+    
+    init(
+        _ viewModel: ReviewDetailViewModel,
+        _ coordinator: ReviewDetailCoordinator
+    ) {
+        self.viewModel = viewModel
+        self.coordinator = coordinator
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
--- MyMoviesInMemory/Presentation/ReviewDetail/ViewModel/ReviewDetailViewModel.swift
+++ MyMoviesInMemory/Presentation/ReviewDetail/ViewModel/ReviewDetailViewModel.swift
@@ -5,31 +5,60 @@
 //  Created by 조성훈 on 2023/01/17.
 //
 
-import Foundation
+import RxSwift
 
 final class ReviewDetailViewModel {
+    
     // MARK: - Input
     
     struct Input {
+        let didShowView: Observable<Void>
     }
     
     // MARK: - Output
     
     struct Output {
+        let reviewWithPoster: Observable<(UIImage?, Review)>
     }
     
     // MARK: - Properties
     
-    
+    private let reviewUseCase: ReviewUseCaseType
+    private let review: Review
+    private let posterImage: UIImage?
     
     // MARK: - Initializer
     
-    init() {
+    init(
+        reviewUseCase: ReviewUseCaseType,
+        review: Review,
+        posterImage: UIImage?
+    ) {
+        self.reviewUseCase = reviewUseCase
+        self.review = review
+        self.posterImage = posterImage
     }
     
     // MARK: - Methods
     
-//    func transform(_ input: Input) -> Output {
-//        
-//    }
+    func transform(_ input: Input) -> Output {
+        let posterImage = input.didShowView
+            .withUnretained(self)
+            .map { owner, _ in
+                owner.posterImage
+            }
+        
+        let review = input.didShowView
+            .withUnretained(self)
+            .map { owner, _ in
+                owner.review
+            }
+        
+        let reviewWithPoster = Observable.combineLatest(
+            posterImage,
+            review
+        )
+        
+        return Output(reviewWithPoster: reviewWithPoster)
+    }
 }
