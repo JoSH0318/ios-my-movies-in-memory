fcee95bf7063accd0c117614a17bfb0e8d778cf5
JoSH0318 <jsh9870@naver.com>
2023-02-13 02:02:38 +0900

    feat: dismissReviewDetailView(), showDeleteAlert() methods 구현

--- MyMoviesInMemory/Presentation/ReviewDetail/Coordinator/ReviewDetailCoordinator.swift
+++ MyMoviesInMemory/Presentation/ReviewDetail/Coordinator/ReviewDetailCoordinator.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RxSwift
 
 final class ReviewDetailCoordinator: Coordinator {
     var navigationController: UINavigationController
@@ -38,5 +39,43 @@ final class ReviewDetailCoordinator: Coordinator {
             reviewDetailViewController,
             animated: true
         )
+    }
+    
+    func dismissReviewDetailView() {
+        navigationController.popViewController(animated: true)
+        parentCoordinator?.removeChildCoordinator(child: self)
+    }
+    
+    func showDeleteAlert() -> Observable<AlertActionType> {
+        return Observable.create { [weak self] emitter in
+            let cancelAction = UIAlertAction(
+                title: "취소",
+                style: .cancel
+            ) { _ in
+                emitter.onNext(.cancel)
+                emitter.onCompleted()
+            }
+
+            let deleteAction = UIAlertAction(
+                title: "삭제",
+                style: .destructive
+            ) { _ in
+                emitter.onNext(.delete)
+                emitter.onCompleted()
+            }
+
+            let alert = AlertBuilder.shared
+                .setType(.alert)
+                .setTitle("삭제할까요?")
+                .setMessage("삭제하면 해당 리뷰는 삭제돼요!")
+                .setActions([cancelAction, deleteAction])
+                .apply()
+            
+            self?.navigationController.present(alert, animated: true)
+            
+            return Disposables.create {
+                alert.dismiss(animated: true)
+            }
+        }
     }
 }
