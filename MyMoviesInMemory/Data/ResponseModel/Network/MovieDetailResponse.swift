99da81b5ee0d942649e6841f4ee17ec8cc8df6d9
JoSH0318 <jsh9870@naver.com>
2023-02-22 03:31:36 +0900

    refactor: response model의 property 타입을 옵셔널로 변경

--- MyMoviesInMemory/Data/ResponseModel/Network/MovieDetailResponse.swift
+++ MyMoviesInMemory/Data/ResponseModel/Network/MovieDetailResponse.swift
@@ -10,21 +10,21 @@ import Foundation
 // MARK: - MovieDetail
 
 struct MovieDetailResponse: Codable {
-    let adult: Bool
-    let backdropPath: String
-    let budget: Int
-    let genres: [Genre]
-    let homepage: String
-    let id: Int
-    let imdbID, originalLanguage, originalTitle, overview: String
-    let popularity: Double
+    let adult: Bool?
+    let backdropPath: String?
+    let budget: Int?
+    let genres: [Genre]?
+    let homepage: String?
+    let id: Int?
+    let imdbID, originalLanguage, originalTitle, overview: String?
+    let popularity: Double?
     let posterPath: String?
-    let releaseDate: String
-    let revenue, runtime: Int
-    let status, tagline, title: String
-    let video: Bool
-    let voteAverage: Double
-    let voteCount: Int
+    let releaseDate: String?
+    let revenue, runtime: Int?
+    let status, tagline, title: String?
+    let video: Bool?
+    let voteAverage: Double?
+    let voteCount: Int?
 
     enum CodingKeys: String, CodingKey {
         case adult
@@ -56,23 +56,23 @@ extension MovieDetailResponse {
         _ actors: String
     ) -> MovieDetail {
         return MovieDetail(
-            id: String(self.id),
-            title: self.title,
-            originalTitle: self.originalTitle,
+            id: String(self.id  ?? 0),
+            title: self.title ?? "",
+            originalTitle: self.originalTitle ?? "",
             posterPath: self.posterPath ?? "",
             genres: selectMainGenres(self.genres),
-            runTime: String(self.runtime),
-            releaseDate: self.releaseDate,
-            userRating: self.voteAverage / 2,
-            overview: self.overview,
+            runTime: String(self.runtime ?? 0),
+            releaseDate: self.releaseDate ?? "",
+            userRating: (self.voteAverage ?? 0.0) / 2 ,
+            overview: self.overview ?? "",
             director: director,
             actors: actors
         )
     }
     
-    private func selectMainGenres(_ genres: [Genre]) -> String {
-        let genresName = genres
-            .map { $0.name }
+    private func selectMainGenres(_ genres: [Genre]?) -> String {
+        guard let genres = genres else { return "" }
+        let genresName = genres.map { $0.name }
         
         guard let firstGenreName = genresName[safe: 0] else { return "" }
         guard let secondGenreName = genresName[safe: 1] else { return "" }
