d4b6d787315e4f8054fef5dba505d5f2b24d284b
JoSH0318 <jsh9870@naver.com>
2023-03-11 03:07:58 +0900

    refactor: ContentTextView 수정 및 추가 구현

--- MyMoviesInMemory/Presentation/Common/View/ContentTextView.swift
+++ MyMoviesInMemory/Presentation/Common/View/ContentTextView.swift
@@ -5,4 +5,51 @@
 //  Created by 조성훈 on 2023/03/11.
 //
 
-import Foundation
+import UIKit
+
+final class ContentTextView: UITextView {
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        setUpTextView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    
+    private func setUpTextView() {
+        inputAccessoryView = makeToolBar()
+        autocorrectionType = .no
+        spellCheckingType = .no
+    }
+    
+    private func makeToolBar() -> UIToolbar {
+        let toolBar = UIToolbar(frame: CGRect(
+            x: .zero,
+            y: .zero,
+            width: UIScreen.main.bounds.size.width,
+            height: 44.0)
+        )
+        let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let barButton = UIBarButtonItem(
+            image: UIImage.keyboardHideButtonImage,
+            style: .plain,
+            target: self,
+            action: #selector(didTapHideButton)
+        )
+        
+        toolBar.items = [flexible, barButton]
+        toolBar.clipsToBounds = true
+        return toolBar
+    }
+    
+    @objc private func didTapHideButton() {
+        self.resignFirstResponder()
+    }
+}
+
